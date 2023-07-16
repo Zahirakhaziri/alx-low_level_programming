9b1ea9a7c50ed07b1a11a53378d1429b96f150ca
Zahirakhaziri <zahirakhazirii@gmail.com>
2023-07-16 12:01:46 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* *
+* main - A program taht print a line using the printf f unction 
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
