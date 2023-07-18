1d0a9fd8237eb86282a784ebba865f6d2c7416ca
Zahirakhaziri <136209620+Zahirakhaziri@users.noreply.github.com>
2023-07-18 03:30:04 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
 /**
-<<<<<<< HEAD
   *main - Entry point
   *Description 'print alphabets except q and e'
   *Return: Always 0
   */
-=======
  * main - Entry point
  * Description 'Print the alphabets excluding q and e'
  *
  * Return: Always 0 (Success)
  */
->>>>>>> 2204bb080d66004f17555774a000a03cdb8d57d8
 int main(void)
 {
 	int n = 97;
