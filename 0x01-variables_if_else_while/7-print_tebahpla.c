73756a2408ec43b03e424beefe9b5b60dd469967
Zahirakhaziri <136209620+Zahirakhaziri@users.noreply.github.com>
2023-07-18 02:49:00 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Print the alphabet in reverse, from 'z' to 'a', followed by a newline character.
+ * Description: Print the alphabet in reverse, from 'z' to 'a'
  *
  * Return: Always 0 (Success)
  */
