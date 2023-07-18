f4ae6a697a122bba6ca162972e99143bbd592429
Zahirakhaziri <136209620+Zahirakhaziri@users.noreply.github.com>
2023-07-18 21:11:35 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - Entry point 
+* main - Entry point .
 * Description: Prints "_putchar" to stdout
 * Return: Always 0
 */
@@ -17,5 +17,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-return 0;
+return (0);
 }
